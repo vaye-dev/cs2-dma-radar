e9c3929e4c34ee50ef9f43f460d8e965513c783b
vaye <54554185+vaye-dev@users.noreply.github.com>
2023-09-30 18:58:15 -0500

    last update remove csgo and update offsets

--- radar/offsets.hpp
+++ radar/offsets.hpp
@@ -3,130 +3,21 @@
 #include <cstddef>
 
 // 2023-06-07 07:51:01.337255200 UTC
-
-#ifndef CS2
-namespace hazedumper {
-	constexpr ::std::int64_t timestamp = 1686124261;
-	namespace netvars {
-		constexpr ::std::ptrdiff_t cs_gamerules_data = 0x0;
-		constexpr ::std::ptrdiff_t m_ArmorValue = 0x117CC;
-		constexpr ::std::ptrdiff_t m_Collision = 0x320;
-		constexpr ::std::ptrdiff_t m_CollisionGroup = 0x474;
-		constexpr ::std::ptrdiff_t m_Local = 0x2FCC;
-		constexpr ::std::ptrdiff_t m_MoveType = 0x25C;
-		constexpr ::std::ptrdiff_t m_OriginalOwnerXuidHigh = 0x31D4;
-		constexpr ::std::ptrdiff_t m_OriginalOwnerXuidLow = 0x31D0;
-		constexpr ::std::ptrdiff_t m_SurvivalGameRuleDecisionTypes = 0x1328;
-		constexpr ::std::ptrdiff_t m_SurvivalRules = 0xD00;
-		constexpr ::std::ptrdiff_t m_aimPunchAngle = 0x303C;
-		constexpr ::std::ptrdiff_t m_aimPunchAngleVel = 0x3048;
-		constexpr ::std::ptrdiff_t m_angEyeAnglesX = 0x117D0;
-		constexpr ::std::ptrdiff_t m_angEyeAnglesY = 0x117D4;
-		constexpr ::std::ptrdiff_t m_bBombDefused = 0x29C0;
-		constexpr ::std::ptrdiff_t m_bBombPlanted = 0x9A5;
-		constexpr ::std::ptrdiff_t m_bBombTicking = 0x2990;
-		constexpr ::std::ptrdiff_t m_bFreezePeriod = 0x20;
-		constexpr ::std::ptrdiff_t m_bGunGameImmunity = 0x9990;
-		constexpr ::std::ptrdiff_t m_bHasDefuser = 0x117DC;
-		constexpr ::std::ptrdiff_t m_bHasHelmet = 0x117C0;
-		constexpr ::std::ptrdiff_t m_bInReload = 0x32B5;
-		constexpr ::std::ptrdiff_t m_bIsDefusing = 0x997C;
-		constexpr ::std::ptrdiff_t m_bIsQueuedMatchmaking = 0x74;
-		constexpr ::std::ptrdiff_t m_bIsScoped = 0x9974;
-		constexpr ::std::ptrdiff_t m_bIsValveDS = 0x7C;
-		constexpr ::std::ptrdiff_t m_bSpotted = 0x93D;
-		constexpr ::std::ptrdiff_t m_bSpottedByMask = 0x980;
-		constexpr ::std::ptrdiff_t m_bStartedArming = 0x3400;
-		constexpr ::std::ptrdiff_t m_bUseCustomAutoExposureMax = 0x9D9;
-		constexpr ::std::ptrdiff_t m_bUseCustomAutoExposureMin = 0x9D8;
-		constexpr ::std::ptrdiff_t m_bUseCustomBloomScale = 0x9DA;
-		constexpr ::std::ptrdiff_t m_clrRender = 0x70;
-		constexpr ::std::ptrdiff_t m_dwBoneMatrix = 0x26A8;
-		constexpr ::std::ptrdiff_t m_fAccuracyPenalty = 0x3340;
-		constexpr ::std::ptrdiff_t m_fFlags = 0x104;
-		constexpr ::std::ptrdiff_t m_flC4Blow = 0x29A0;
-		constexpr ::std::ptrdiff_t m_flCustomAutoExposureMax = 0x9E0;
-		constexpr ::std::ptrdiff_t m_flCustomAutoExposureMin = 0x9DC;
-		constexpr ::std::ptrdiff_t m_flCustomBloomScale = 0x9E4;
-		constexpr ::std::ptrdiff_t m_flDefuseCountDown = 0x29BC;
-		constexpr ::std::ptrdiff_t m_flDefuseLength = 0x29B8;
-		constexpr ::std::ptrdiff_t m_flFallbackWear = 0x31E0;
-		constexpr ::std::ptrdiff_t m_flFlashDuration = 0x10470;
-		constexpr ::std::ptrdiff_t m_flFlashMaxAlpha = 0x1046C;
-		constexpr ::std::ptrdiff_t m_flLastBoneSetupTime = 0x2928;
-		constexpr ::std::ptrdiff_t m_flLowerBodyYawTarget = 0x9ADC;
-		constexpr ::std::ptrdiff_t m_flNextAttack = 0x2D80;
-		constexpr ::std::ptrdiff_t m_flNextPrimaryAttack = 0x3248;
-		constexpr ::std::ptrdiff_t m_flSimulationTime = 0x268;
-		constexpr ::std::ptrdiff_t m_flTimerLength = 0x29A4;
-		constexpr ::std::ptrdiff_t m_hActiveWeapon = 0x2F08;
-		constexpr ::std::ptrdiff_t m_hBombDefuser = 0x29C4;
-		constexpr ::std::ptrdiff_t m_hMyWeapons = 0x2E08;
-		constexpr ::std::ptrdiff_t m_hObserverTarget = 0x339C;
-		constexpr ::std::ptrdiff_t m_hOwner = 0x29DC;
-		constexpr ::std::ptrdiff_t m_hOwnerEntity = 0x14C;
-		constexpr ::std::ptrdiff_t m_hViewModel = 0x3308;
-		constexpr ::std::ptrdiff_t m_iAccountID = 0x2FD8;
-		constexpr ::std::ptrdiff_t m_iClip1 = 0x3274;
-		constexpr ::std::ptrdiff_t m_iCompetitiveRanking = 0x1A84;
-		constexpr ::std::ptrdiff_t m_iCompetitiveWins = 0x1B88;
-		constexpr ::std::ptrdiff_t m_iCrosshairId = 0x11838;
-		constexpr ::std::ptrdiff_t m_iDefaultFOV = 0x333C;
-		constexpr ::std::ptrdiff_t m_iEntityQuality = 0x2FBC;
-		constexpr ::std::ptrdiff_t m_iFOV = 0x31F4;
-		constexpr ::std::ptrdiff_t m_iFOVStart = 0x31F8;
-		constexpr ::std::ptrdiff_t m_iGlowIndex = 0x10488;
-		constexpr ::std::ptrdiff_t m_iHealth = 0x100;
-		constexpr ::std::ptrdiff_t m_iItemDefinitionIndex = 0x2FBA;
-		constexpr ::std::ptrdiff_t m_iItemIDHigh = 0x2FD0;
-		constexpr ::std::ptrdiff_t m_iMostRecentModelBoneCounter = 0x2690;
-		constexpr ::std::ptrdiff_t m_iObserverMode = 0x3388;
-		constexpr ::std::ptrdiff_t m_iShotsFired = 0x103E0;
-		constexpr ::std::ptrdiff_t m_iState = 0x3268;
-		constexpr ::std::ptrdiff_t m_iTeamNum = 0xF4;
-		constexpr ::std::ptrdiff_t m_lifeState = 0x25F;
-		constexpr ::std::ptrdiff_t m_nBombSite = 0x2994;
-		constexpr ::std::ptrdiff_t m_nFallbackPaintKit = 0x31D8;
-		constexpr ::std::ptrdiff_t m_nFallbackSeed = 0x31DC;
-		constexpr ::std::ptrdiff_t m_nFallbackStatTrak = 0x31E4;
-		constexpr ::std::ptrdiff_t m_nForceBone = 0x268C;
-		constexpr ::std::ptrdiff_t m_nModelIndex = 0x258;
-		constexpr ::std::ptrdiff_t m_nTickBase = 0x3440;
-		constexpr ::std::ptrdiff_t m_nViewModelIndex = 0x29D0;
-		constexpr ::std::ptrdiff_t m_rgflCoordinateFrame = 0x444;
-		constexpr ::std::ptrdiff_t m_szCustomName = 0x304C;
-		constexpr ::std::ptrdiff_t m_szLastPlaceName = 0x35C4;
-		constexpr ::std::ptrdiff_t m_thirdPersonViewAngles = 0x31E8;
-		constexpr ::std::ptrdiff_t m_vecOrigin = 0x138;
-		constexpr ::std::ptrdiff_t m_vecVelocity = 0x114;
-		constexpr ::std::ptrdiff_t m_vecViewOffset = 0x108;
-		constexpr ::std::ptrdiff_t m_viewPunchAngle = 0x3030;
-		constexpr ::std::ptrdiff_t m_zoomLevel = 0x33E0;
-	} // namespace netvars
-}
-
-using namespace hazedumper;
-#else// CS2
 namespace offset {
 	// credits: https://github.com/UnnamedZ03/CS2-external-base/blob/main/source/CSSPlayer.hpp
+	constexpr DWORD64 entity_list_offset = 0x178c8a8;
+	constexpr DWORD64 view_matrix_offset = 0x187a700;
+	constexpr DWORD64 local_player_offset = 0x17db128;
+	constexpr DWORD64 globals_offset = 0x168fce8;
 
-	/* @NOTE: I retrieved this with a little reversing, nothing complicated and I regularly post new ones. */
-	constexpr DWORD64 s_dwEntityList_Offset = 0x1624C78;
-	constexpr DWORD64 viewmatrix_Offset = 0x1714C70;
-	constexpr DWORD64 localplayer_Offset = 0x1715418;
-	
-	constexpr DWORD64 globals_Offset = 0x1556718;
+	constexpr DWORD64 m_iPawnHealth = 0x808;
+	constexpr DWORD64 m_hPawn = 0x5dc;
+	constexpr DWORD64 m_sSanitizedPlayerName = 0x720;
+	constexpr DWORD64 m_vOldOrigin = 0x1214;
+	constexpr DWORD64 m_iTeamNum = 0x3bf;
+	constexpr DWORD64 m_bIsLocalPlayerController = 0x6A0;
 
-	/* @NOTE: You can obtain such offsets by using source2gen */
-	constexpr DWORD64 s_dwPawnIsAlive_Offset = 0x804;
-	constexpr DWORD64 s_dwPawnHealth_Offset = 0x808;
-	constexpr DWORD64 s_dwPlayerPawn_Offset = 0x5dc;
-	constexpr DWORD64 s_dwSanitizedName_Offset = 0x720;
-	constexpr DWORD64 s_Position_Offset = 0x12AC;
-	constexpr DWORD64 s_teamnum_Offset = 0x3bf;
-	constexpr DWORD64 s_bIsLocalPlayerController_Offset = 0x6A0;
-
-	inline DWORD64 g_dwEntList = 0;
+	inline DWORD64 entity_list = 0;
 	inline DWORD64 client = 0;
 
 	class CGlobalVarsBase {
@@ -147,4 +38,3 @@ namespace offset {
 		std::uint64_t current_map_name;
 	};
 }
-#endif
--- radar/radar.cpp
+++ radar/radar.cpp
@@ -47,9 +47,8 @@ bool connect() {
 	return true;
 }
 
-#ifdef CS2
 void setup_csgo() {
-	std::cout << "Waiting for Counter-Strike: Global Offensive..." << std::endl;
+	std::cout << "Waiting for Counter-Strike 2..." << std::endl;
 	if (!process->init("cs2.exe"))
 		return;
 
@@ -62,27 +61,25 @@ void setup_csgo() {
 	}
 
 	auto client = process->client();
-
-	std::thread([]() {
-		while (true) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
-			std::cout << "enter map name (de_dust2, de_mirage, de_overpass, etc.): ";
-			std::cin >> map_name;
-		}
-	}).detach();
+	auto pglobals = process->read<uint64_t>(offset::globals_offset);
 
 	connect();
 	while (true) {
-		const auto local_player = process->read<uint64_t>(client + offset::localplayer_Offset);
+		const auto local_player = process->read<uint64_t>(client + offset::local_player_offset);
 		if (!local_player)
 			continue;
 
-		offset::g_dwEntList = process->read<uint64_t>(client + offset::s_dwEntityList_Offset);
-		if (!offset::g_dwEntList)
+		const auto globals = process->read<offset::CGlobalVarsBase>(pglobals);
+		const auto map_name = process->read_string(globals.current_map_name, 32);
+		if (map_name == "<empty>")
+			continue;
+
+		offset::entity_list = process->read<uint64_t>(client + offset::entity_list_offset);
+		if (!offset::entity_list)
 			continue;
 
 		static uint64_t first_player_controller = 0;
-		if (first_player_controller == 0 || process->read<int>(local_player + offset::s_dwPawnHealth_Offset) <= 0) {
+		if (first_player_controller == 0 || process->read<int>(local_player + offset::m_iPawnHealth) <= 0) {
 			for (int i = 0; i < 32; i++) {
 				const auto entity = get_entity(i);
 				if (!entity)
@@ -112,14 +109,14 @@ void setup_csgo() {
 		int i = -1;
 		nlohmann::json j;
 		j["global"]["map"] = map_name;
-		j["global"]["team"] = process->read<int>(local_player + offset::s_teamnum_Offset);
+		j["global"]["team"] = process->read<int>(local_player + offset::m_iTeamNum);
 		for (; player_controller; player_controller = get_next_entity(process->read<uint64_t>(player_controller + 0x10))) {
 			i++;
-			const auto pawn = get_entity_pawn(process->read<uint64_t>(player_controller + offset::s_dwPlayerPawn_Offset));
+			const auto pawn = get_entity_pawn(process->read<uint64_t>(player_controller + offset::m_hPawn));
 			if (!pawn)
 				continue;
 
-			const auto health = process->read<int>(player_controller + offset::s_dwPawnHealth_Offset);
+			const auto health = process->read<int>(player_controller + offset::m_iPawnHealth);
 			if (health <= 0 || health > 100)
 				continue;
 
@@ -127,13 +124,13 @@ void setup_csgo() {
 			if (!name_ptr)
 				continue;
 
-			auto pos = process->read<pos_t>(pawn + offset::s_Position_Offset);
+			auto pos = process->read<pos_t>(pawn + offset::m_vOldOrigin);
 			auto _i = std::to_string(i);
 			j[_i]["health"] = health;
-			j[_i]["local"] = process->read<bool>(player_controller + offset::s_bIsLocalPlayerController_Offset);
+			j[_i]["local"] = process->read<bool>(player_controller + offset::m_bIsLocalPlayerController);
 			j[_i]["name"] = process->read_string(name_ptr, 32);
 			j[_i]["position"] = { pos.x, pos.y };
-			j[_i]["team"] = process->read<int>(player_controller + offset::s_teamnum_Offset);
+			j[_i]["team"] = process->read<int>(player_controller + offset::m_iTeamNum);
 			j[_i]["weapon"] = "unknown";
 		}
 
@@ -153,146 +150,6 @@ void setup_csgo() {
 	}
 }
 
-#else // CS2
-void setup_csgo() {
-	std::cout << "Waiting for Counter-Strike: Global Offensive..." << std::endl;
-	if (!process->init("csgo.exe"))
-		return;
-
-	std::cout << "Waiting for serverbrowser.dll..." << std::endl;
-	while (!process->get_module_base(L"serverbrowser.dll"))
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-
-	std::cout << "serverbrowser.dll found!" << std::endl;
-
-	std::cout << "Trying to get necessary modules..." << std::endl;
-
-	while (!process->client() || !process->engine()) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
-		if (!process->client())
-			process->client() = process->get_module_base(L"client.dll");
-
-		if (!process->engine())
-			process->engine() = process->get_module_base(L"engine.dll");
-	}
-
-	auto client = process->client();
-	auto client_size = process->get_module_size(L"client.dll");
-
-	auto engine = process->engine();
-	auto engine_size = process->get_module_size(L"engine.dll");
-
-	std::cout << "All necessary modules found!" << std::endl;
-	std::cout << "csgo.exe = 0x" << std::hex << process->process_base() << std::endl;
-	std::cout << "client.dll = 0x" << std::hex << client << std::endl;
-	std::cout << "engine.dll = 0x" << std::hex << engine << std::endl;
-
-	auto local_player_ptr = process->scan(client, client_size, "\x8D\x34\x85\x00\x00\x00\x00\x89\x15\x00\x00\x00\x00\x8B\x41\x08\x8B\x48\x04\x83\xF9\xFF", "xxx????xx????xxxxxxxxx") + 3;
-	local_player_ptr = process->read<uint32_t>(local_player_ptr) + 4;
-
-	auto client_state = process->scan(engine, engine_size, "\xA1\x00\x00\x00\x00\x33\xD2\x6A\x00\x6A\x00\x33\xC9\x89\xB0", "x????xxxxxxxxxx") + 1;
-	client_state = process->read<uint32_t>(client_state);
-
-	auto client_state_map = process->scan(engine, engine_size, "\x05\x00\x00\x00\x00\xC3\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xA1", "x????xxxxxxxxx") + 1;
-	client_state_map = process->read<uint32_t>(client_state_map);
-
-	auto entity_list = process->scan(client, client_size, "\xBB\x00\x00\x00\x00\x83\xFF\x01\x0F\x8C\x00\x00\x00\x00\x3B\xF8", "x????xxxxx????xx") + 1;
-	entity_list = process->read<uint32_t>(entity_list);
-
-	auto radar_base = process->scan(client, client_size, "\xA1\x00\x00\x00\x00\x8B\x0C\xB0\x8B\x01\xFF\x50\x00\x46\x3B\x35\x00\x00\x00\x00\x7C\xEA\x8B\x0D", "x????xxxxxxx?xxx????xxxx") + 1;
-	radar_base = process->read<uint32_t>(radar_base);
-
-	auto dormant = 0xED;
-
-	std::cout << "local_player = 0x" << std::hex << local_player_ptr << "(0x" << local_player_ptr - client << ")" << std::endl;
-	std::cout << "client_state = 0x" << std::hex << client_state << "(0x" << client_state - engine << ")" << std::endl;
-	std::cout << "client_state_map = 0x" << std::hex << client_state_map << std::endl;
-	std::cout << "entity_list = 0x" << std::hex << entity_list << "(0x" << entity_list - client << ")" << std::endl;
-	std::cout << "radar_base = 0x" << std::hex << radar_base << "(0x" << radar_base - client << ")" << std::endl;
-	std::cout << "dormant = 0x" << std::hex << dormant << std::endl;
-
-	while (true) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		if (GetAsyncKeyState(VK_END))
-			break;
-
-		auto map = process->read_string(process->read<uint32_t>(client_state) + client_state_map, 64);
-		if (map.empty())
-			continue;
-
-		const auto local_player = process->read<uint32_t>(local_player_ptr);
-		if (!local_player)
-			continue;
-
-		nlohmann::json j;
-		j["global"]["map"] = map.c_str();
-		j["global"]["team"] = process->read<int>(local_player + netvars::m_iTeamNum);
-		for (auto i = 0; i <= 32; i++) {
-			const auto entity = process->read<uint32_t>(entity_list + 0x10 * i);
-			if (!entity)
-				continue;
-
-			const auto health = process->read<int>(entity + netvars::m_iHealth);
-			if (!health)
-				continue;
-
-			const auto lifestate = process->read<uint8_t>(entity + netvars::m_lifeState);
-			if (lifestate)
-				continue;
-
-			const auto bdormant = process->read<uint8_t>(entity + dormant);
-			if (bdormant)
-				continue;
-
-			const auto active_weapon = process->read<uint32_t>(entity + netvars::m_hActiveWeapon);
-			if (!active_weapon)
-				continue;
-
-			const auto weapon_entity = process->read<uint32_t>(entity_list + ((active_weapon & 0xFFF) - 1) * 0x10);
-			if (!weapon_entity)
-				continue;
-
-			auto weapon_index = process->read<short>(weapon_entity + netvars::m_iItemDefinitionIndex);
-			if (weapon_index > 64 || weapon_index < 1)
-				weapon_index = 42;
-
-			auto radar = process->read<uint32_t>(process->read<uint32_t>(radar_base) + 0x78);
-			if (!radar)
-				continue;
-
-			auto name = process->read_string(radar + 0x174 * (i + 2) + 0x18, 128);
-			if (name.empty())
-				continue;
-
-			struct pos_t {
-				float x, y;
-			} pos = process->read<pos_t>(entity + netvars::m_vecOrigin);
-
-			const auto _i = std::to_string(i);
-			j[_i]["health"] = health;
-			j[_i]["local"] = local_player == entity;
-			j[_i]["name"] = name.c_str();
-			j[_i]["position"] = { pos.x, pos.y };
-			j[_i]["team"] = process->read<int>(entity + netvars::m_iTeamNum);
-			j[_i]["weapon"] = strcmp(weapon_names[weapon_index], "none") ? weapon_names[weapon_index] : "KNIFE";
-		}
-
-		DWORD write_buffer_written_bytes;
-		const auto pipe_write = WriteFile(pipe, j.dump().c_str(), j.dump().size(), &write_buffer_written_bytes, nullptr);
-		if (!pipe_write) {
-			std::cout << "pipe_write false = " << std::dec << GetLastError() << std::endl;
-			if (GetLastError() == 232) {
-				DisconnectNamedPipe(pipe);
-				CloseHandle(pipe);
-				connect();
-			}
-		}
-
-		if (auto pipe_flush = FlushFileBuffers(pipe); !pipe_flush)
-			std::cout << "pipe_flush false = " << std::dec << GetLastError() << std::endl;
-	}
-}
-#endif
 
 int main() {
 	setup_csgo();
